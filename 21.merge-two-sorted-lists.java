0d575c1a474695c8c809c82ea38005c260ee0618
errorlike <errorlikeman@outlook.com>
2023-08-19 01:35:21 +0800

    21 merge-two-sorted

---
+++ 21.merge-two-sorted-lists.java
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.cn/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (66.30%)
+ * Likes:    3238
+ * Dislikes: 0
+ * Total Accepted:    1.5M
+ * Total Submissions: 2.2M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * 
+ * Merge the two lists into one sorted list. The list should be made by
+ * splicing together the nodes of the first two lists.
+ * 
+ * Return the head of the merged linked list.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: list1 = [], list2 = []
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in both lists is in the range [0, 50].
+ * -100 <= Node.val <= 100
+ * Both list1 and list2 are sorted in non-decreasing order.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// todo 终止条件 l1或者l2为空。
+// 使用递归的方式，将l1 l2的合并问题，转换成，l1->next与l2的合并问题，
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        } else if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
+// @lc code=end
